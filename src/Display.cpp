d0949448044e2e1b47b544db109c0161ac811887
redstonee <1516235445@qq.com>
2025-01-24 20:49:58 +0800

    Use PIO SPI for the screen, fix some fuck-ups in display

--- src/Display.cpp
+++ src/Display.cpp
@@ -6,11 +6,11 @@
 
 namespace Display
 {
-    TFT_eSPI screen;
+    static TFT_eSPI screen;
     std::function<void(uint32_t *, bool *)> readKeyEventCb;
 
-    void flushDisplay(lv_display_t *disp, const lv_area_t *area,
-                      uint8_t *px_map)
+    static void flushDisplay(lv_display_t *disp, const lv_area_t *area,
+                             uint8_t *px_map)
     {
         uint32_t w = lv_area_get_width(area);
         uint32_t h = lv_area_get_height(area);
@@ -23,8 +23,13 @@ namespace Display
         lv_disp_flush_ready(disp);
     }
 
-    void readKey(lv_indev_t *indev, lv_indev_data_t *data)
+    static void readKey(lv_indev_t *indev, lv_indev_data_t *data)
     {
+        if (!readKeyEventCb)
+        {
+            ULOG_WARNING("readKeyEventCb not set");
+            return;
+        }
         bool pressed;
         readKeyEventCb(&data->key, &pressed);
         data->state = pressed ? LV_INDEV_STATE_PRESSED : LV_INDEV_STATE_RELEASED;
@@ -35,7 +40,7 @@ namespace Display
         readKeyEventCb = cb;
     }
 
-    void printLog(lv_log_level_t level, const char *buf)
+    static void printLog(lv_log_level_t level, const char *buf)
     {
         constexpr ulog_level_t ULOG_LEVELS[]{
             ULOG_TRACE_LEVEL, ULOG_INFO_LEVEL, ULOG_WARNING_LEVEL,
@@ -55,19 +60,23 @@ namespace Display
 
         lv_log_register_print_cb(printLog);
 
-        static uint32_t draw_buf[TFT_WIDTH * TFT_WIDTH / 10];
-        auto disp = lv_display_create(TFT_WIDTH, TFT_WIDTH);
+        static uint32_t drawBuf[TFT_WIDTH * TFT_HEIGHT / 10];
+        auto disp = lv_display_create(TFT_WIDTH, TFT_HEIGHT);
         lv_display_set_flush_cb(disp, flushDisplay);
-        lv_display_set_buffers(disp, draw_buf, NULL, sizeof(draw_buf),
+        lv_display_set_buffers(disp, drawBuf, NULL, sizeof(drawBuf),
                                LV_DISPLAY_RENDER_MODE_PARTIAL);
 
         lv_indev_t *indev = lv_indev_create();
         lv_indev_set_type(indev, LV_INDEV_TYPE_KEYPAD);
         lv_indev_set_read_cb(indev, readKey);
+
+        auto *label = lv_label_create(lv_screen_active());
+        lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+        lv_label_set_text(label, "Fuck my ass!!!!");
     }
 
     void handleTimer()
     {
-        lv_timer_handler();
+        delay(lv_timer_handler());
     }
 } // namespace display
